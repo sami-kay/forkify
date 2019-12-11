17c09139f135bc03518fcdc2f907608824c7f10a
sami-kay <54155434+sami-kay@users.noreply.github.com>
2019-12-11 10:26:39 -0600

    fixed bug in imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
-import Likes from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
